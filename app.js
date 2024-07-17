5d40f65f78c1ec4493a9b64c0a792dd8485c32e8
leticiabrum <leticiabrumcorrealeite@gmail.com>
2024-07-17 14:19:13 -0300

    ending(sortear-numeros)

--- app.js
+++ app.js
@@ -1,46 +1,61 @@
-let textoNumerosSorteados = document.getElementById('resultado');
-let desabilitedButton = document.getElementById('btn-reiniciar');
 
 
 function sortear() {
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let numeroInicial = parseInt(document.getElementById('de').value);
     let numeroLimite = parseInt(document.getElementById('ate').value);
-
     let sorteados = [];
     let numero;
 
     for (let i = 0; i < quantidade; i++) {
         numero = sortearNumeroAleatorio(numeroInicial, numeroLimite);
-
+        
         while (sorteados.includes(numero)) {
           numero = sortearNumeroAleatorio(numeroInicial, numeroLimite);   
         }
-
         sorteados.push(numero);
     }
-
-   textoNumerosSorteados.textContent = sorteados.length > 1 && sorteados.length > 1 ? `Números sorteados: ${sorteados}` : `Número 
-   sorteado: ${sorteados}`;
-
-   desabilitedButton.classList.remove('desabilitado');
+    
+    console.log(numero);
+    textPluralSingular(sorteados);
 
 }
 
+function textoPluralSingular(sorteados) {
+    let textoNumerosSorteados = document.getElementById('resultado');
+    let textoPlural = `Numeros sorteados: ${sorteados}`;
+    let textoSingular = `Numero sorteado: ${sorteados}`;
+    if (sorteados.length > 1) {
+        textoNumerosSorteados.textContent = textoPlural;
+    } else {
+        textoNumerosSorteados.textContent = textoSingular;
+    }
+}
+
+function changeButtonStatus() {
+    let button = document.getElementById('btn-reiniciar');
+    if (button.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
 
 function sortearNumeroAleatorio(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
 function reiniciar() {
     let quantidade = document.getElementById('quantidade');
-    quantidade.value = "";
+    quantidade.value = '';
     let numeroInicial = document.getElementById('de');
-    numeroInicial.value = "";
+    numeroInicial.value = '';
     let numeroLimite = document.getElementById('ate');
-    numeroLimite.value = "";
+    numeroLimite.value = '';
 
     textoNumerosSorteados.textContent = 'Números sorteados: nenhum até agora';
-    desabilitedButton.classList.add('desabilitado');
+
+    changeButtonStatus();
 }
